1d895576e5a5d6182d4016a9059cb0327b5d6652
Jim Vallandingham <vlandham@gmail.com>
2015-07-16 11:31:02 -0700

    Add first sections

---
+++ sections/a1_setup/commands.sh
@@ -0,0 +1,10 @@
+
+# install gitx
+# http://rowanj.github.io/gitx/
+
+# install hub
+https://github.com/github/hub
+
+# first fork repo to moebiolabs
+
+git clone git@github.com:moebiolabs/git_practice.git
---
+++ sections/b1_feature_branches/commands.sh
@@ -0,0 +1,32 @@
+
+# ensure up-to-date
+git pull origin master
+
+# create new branch
+git checkout -b add_new_feature
+
+# make changes
+touch src/feature.js
+echo "var i = 0;" > src/feature.js
+
+# add changes
+git add src/feature.js
+
+# commit changes
+git commit -m 'Add feature file'
+
+# see current status
+git status
+
+# see change history
+git log
+
+# gitx
+gitx .
+
+
+# push to remote branch
+git push origin add_new_feature
+
+# go to repo
+hub browse
